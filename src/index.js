b06df1dceea82a4923e191a6cd2974a41ef3ccb0
Greg Geary <ggeary@wlgore.com>
2024-05-06 12:14:49 +1000

    update gitignore

--- src/index.js
+++ src/index.js
@@ -7,12 +7,24 @@ const PORT = process.env.PORT || 3000; // uses 8080 for local dev
 app.use(express.json());
 
 app.get("/test", (req, res) => {
+  // do stuff to get data
   return res.status(200).send("hello world");
 });
 
 app.post("/test/:queryParam", (req, res) => {
   const { queryParam } = req.params;
   const { data } = req.body;
+  // do stuff to create data
+  return res.status(200).send({
+    queryParam: queryParam,
+    body: data,
+  });
+});
+
+app.put("/test/:queryParam", (req, res) => {
+  const { queryParam } = req.params;
+  const { data } = req.body;
+  // do stuff to update data
   return res.status(200).send({
     queryParam: queryParam,
     body: data,
